6df5b4749e724c9d73907b01dd256ee73540a847
Pedro Stefano <pedro.fzs@hotmail.com>
2020-11-14 12:14:14 -0300

    Finalizando o Desafio: GoFinances Web

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -30,12 +30,45 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  function verifyValue(value: string, type: 'income' | 'outcome'): string {
+    let newValue = value;
+
+    if (type === 'outcome') {
+      newValue = `- ${value}`;
+    }
+
+    return newValue;
+  }
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get('/transactions');
+      const {
+        transactions: newTransactionsList,
+        balance: newBalance,
+      } = response.data;
+
+      const transactionsFormatted = newTransactionsList.map(
+        (tran: Transaction) => ({
+          ...tran,
+          value: verifyValue(formatValue(tran.value), tran.type),
+          created_at: Intl.DateTimeFormat('pt-BR', {
+            timeZone: 'UTC',
+          }).format(new Date(tran.created_at)),
+        }),
+      );
+
+      const balanceFormatted = {
+        income: formatValue(Number(newBalance.income)),
+        outcome: formatValue(Number(newBalance.outcome)),
+        total: formatValue(Number(newBalance.total)),
+      };
+
+      setTransactions(transactionsFormatted);
+      setBalance(balanceFormatted);
     }
 
     loadTransactions();
@@ -51,21 +84,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +114,19 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions &&
+                transactions.map(transaction => (
+                  <tr key={transaction.id}>
+                    <td className="title">{transaction.title}</td>
+                    {transaction.type === 'income' ? (
+                      <td className="income">{transaction.value}</td>
+                    ) : (
+                      <td className="outcome">{transaction.value}</td>
+                    )}
+                    <td>{transaction.category.title}</td>
+                    <td>{transaction.created_at}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </TableContainer>
